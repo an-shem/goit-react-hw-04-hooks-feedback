edd7a262f19f2be694103223750acffc934e680c
Andrey Shemet <an-shem@ukr.net>
2021-08-01 00:11:04 +0300

    поправил стили

--- src/components/AppFeedback/App.styled.jsx
+++ src/components/AppFeedback/App.styled.jsx
@@ -5,7 +5,7 @@ export const AppWrap = styled.div`
   display: flex;
   flex-direction: column;
   width: 600px;
-  margin: 40px auto 0;
+  margin: 10px auto 0;
   border: 1px solid #c9c1c1;
   border-radius: 30px;
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
